fcee28da6416be5112bedc2908ab3a7866b8fa54
bhasharm7 <bhasharm7@publicisgroupe.net>
2019-05-30 14:44:33 +0530

    Scala basic code tutorial

---
+++ src/main/scala/exercise/FunctionExercise.scala
@@ -0,0 +1,59 @@
+package exercise
+
+
+/**
+  * Exercise 1
+  *
+  * */
+object FunctionExercise extends App {
+
+  /*
+   *  1.  A greeting function (name, age) => "Hi, my name is $name and I am $age years old."
+   *
+   **/
+
+  println(aGreetingFunction("Rahul", 22))
+
+  def aGreetingFunction(name: String, age: Int): String = {
+    s"Hi, My name is $name and I am $age year old"
+  }
+
+  /*
+   *  2.  Factorial function 1 * 2 * 3 * .. * n
+   *
+   * */
+
+  println(aFactorialFunction(5))
+  def aFactorialFunction(n: Int): Int = {
+    if (n <= 0) 1
+    else n * aFactorialFunction(n - 1)
+  }
+
+  /** 3.  A Fibonacci function
+      f(1) = 1
+    f(2) = 1
+    f(n) = f(n - 1) + f(n - 2)
+    * */
+
+  println(9)
+  def aFibonacciFunction(n: Int): Int = {
+    if (n <= 2) 1
+    else aFibonacciFunction(n - 1) + aFibonacciFunction(n - 2)
+  }
+
+  /**
+    * Prime number calculation
+    *
+    * */
+  def isPrime(n:Int):Boolean ={
+    def isPrimeUtil(t:Int): Boolean =
+      if(t <= 1) true
+      else n % t != 0 && isPrimeUtil(t-1)
+
+    isPrimeUtil(n / 2)
+  }
+
+  println("  "+isPrime(37))
+  println(isPrime(2003))
+  println(isPrime(37 * 17))
+}
---
+++ src/main/scala/exercise/TailRecursionExercise.scala
@@ -0,0 +1,56 @@
+package exercise
+
+object TailRecursionExercise extends App {
+
+  /*
+   1.  Concatenate a string n times
+   2.  IsPrime function tail recursive
+   3.  Fibonacci function, tail recursive.
+  */
+
+  /**
+    * Concatenate a String n times
+    **/
+  def concatenateStringTail(aString: String, n: Int): String = {
+    def conTailRec(aStr: String, x: Int, accumlator: String): String =
+      if (x <= 0) accumlator
+      else
+        conTailRec(aStr, x - 1, aStr + accumlator)
+
+    conTailRec(aString, n, "")
+  }
+
+  println(concatenateStringTail("hello", 3))
+
+
+  /**
+    *   2.  IsPrime function tail recursive
+    **/
+
+  def isPrime(n: Int): Boolean = {
+    def primeCheck(t: Int, accumulator: Boolean): Boolean =
+      if (!accumulator) false
+      else if (t <= 1) true
+      else primeCheck(t - 1, n % 2 != 0 && accumulator)
+
+    primeCheck(n / 2, true)
+  }
+
+  println(isPrime(2103))
+  println(isPrime(629))
+
+
+  /**
+    *     3.  Fibonacci function, tail recursive.
+    **/
+  def fibonacci(n: Int): Int = {
+    def fiboTailrec(i: Int, last: Int, nextToLast: Int): Int =
+      if(i >= n) last
+      else fiboTailrec(i + 1, last + nextToLast, last)
+
+    if (n <= 2) 1
+    else fiboTailrec(2, 1, 1)
+  }
+
+  println(fibonacci(8)) // 1 1 2 3 5 8 13, 21
+}
